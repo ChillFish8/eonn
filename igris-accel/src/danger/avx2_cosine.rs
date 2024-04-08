9b06489a872181fbc944de575ca8439221869c85
ChillFish8 <hburt2003@gmail.com>
2024-04-08 11:30:37 +0100

    Restructure code

--- igris-accel/src/danger/avx2_cosine.rs
+++ igris-accel/src/danger/avx2_cosine.rs
@@ -3,188 +3,6 @@ use std::arch::x86_64::*;
 use crate::danger::{offsets, rollup_x4, sum_avx2};
 use crate::math::{FastMath, Math, StdMath};
 
-#[inline(always)]
-unsafe fn cosine<M: Math>(acc: __m256, norm_x_acc: __m256, norm_y_acc: __m256) -> f32 {
-    let dot_product = sum_avx2(acc);
-    let norm_x = sum_avx2(norm_x_acc);
-    let norm_y = sum_avx2(norm_y_acc);
-
-    if norm_x == 0.0 && norm_y == 0.0 {
-        0.0
-    } else if norm_x == 0.0 || norm_y == 0.0 {
-        1.0
-    } else {
-        M::sub(1.0, M::div(dot_product, M::mul(norm_x, norm_y).sqrt()))
-    }
-}
-
-#[allow(clippy::too_many_arguments)]
-#[inline(always)]
-unsafe fn execute_f32_x64_nofma_block_cosine(
-    x: *const f32,
-    y: *const f32,
-    acc1: &mut __m256,
-    acc2: &mut __m256,
-    acc3: &mut __m256,
-    acc4: &mut __m256,
-    norm_x_acc1: &mut __m256,
-    norm_x_acc2: &mut __m256,
-    norm_x_acc3: &mut __m256,
-    norm_x_acc4: &mut __m256,
-    norm_y_acc1: &mut __m256,
-    norm_y_acc2: &mut __m256,
-    norm_y_acc3: &mut __m256,
-    norm_y_acc4: &mut __m256,
-) {
-    let [x1, x2, x3, x4] = offsets(x, 0);
-    let [x5, x6, x7, x8] = offsets(x, 32);
-
-    let [y1, y2, y3, y4] = offsets(y, 0);
-    let [y5, y6, y7, y8] = offsets(y, 32);
-
-    let x1 = _mm256_loadu_ps(x1);
-    let x2 = _mm256_loadu_ps(x2);
-    let x3 = _mm256_loadu_ps(x3);
-    let x4 = _mm256_loadu_ps(x4);
-    let x5 = _mm256_loadu_ps(x5);
-    let x6 = _mm256_loadu_ps(x6);
-    let x7 = _mm256_loadu_ps(x7);
-    let x8 = _mm256_loadu_ps(x8);
-
-    let y1 = _mm256_loadu_ps(y1);
-    let y2 = _mm256_loadu_ps(y2);
-    let y3 = _mm256_loadu_ps(y3);
-    let y4 = _mm256_loadu_ps(y4);
-    let y5 = _mm256_loadu_ps(y5);
-    let y6 = _mm256_loadu_ps(y6);
-    let y7 = _mm256_loadu_ps(y7);
-    let y8 = _mm256_loadu_ps(y8);
-
-    let r1 = _mm256_mul_ps(x1, y1);
-    let r2 = _mm256_mul_ps(x2, y2);
-    let r3 = _mm256_mul_ps(x3, y3);
-    let r4 = _mm256_mul_ps(x4, y4);
-    let r5 = _mm256_mul_ps(x5, y5);
-    let r6 = _mm256_mul_ps(x6, y6);
-    let r7 = _mm256_mul_ps(x7, y7);
-    let r8 = _mm256_mul_ps(x8, y8);
-
-    let norm_x_r1 = _mm256_mul_ps(x1, x1);
-    let norm_x_r2 = _mm256_mul_ps(x2, x2);
-    let norm_x_r3 = _mm256_mul_ps(x3, x3);
-    let norm_x_r4 = _mm256_mul_ps(x4, x4);
-    let norm_x_r5 = _mm256_mul_ps(x5, x5);
-    let norm_x_r6 = _mm256_mul_ps(x6, x6);
-    let norm_x_r7 = _mm256_mul_ps(x7, x7);
-    let norm_x_r8 = _mm256_mul_ps(x8, x8);
-
-    let norm_y_r1 = _mm256_mul_ps(y1, y1);
-    let norm_y_r2 = _mm256_mul_ps(y2, y2);
-    let norm_y_r3 = _mm256_mul_ps(y3, y3);
-    let norm_y_r4 = _mm256_mul_ps(y4, y4);
-    let norm_y_r5 = _mm256_mul_ps(y5, y5);
-    let norm_y_r6 = _mm256_mul_ps(y6, y6);
-    let norm_y_r7 = _mm256_mul_ps(y7, y7);
-    let norm_y_r8 = _mm256_mul_ps(y8, y8);
-
-    *acc1 = _mm256_add_ps(*acc1, r1);
-    *acc2 = _mm256_add_ps(*acc2, r2);
-    *acc3 = _mm256_add_ps(*acc3, r3);
-    *acc4 = _mm256_add_ps(*acc4, r4);
-    *acc1 = _mm256_add_ps(*acc1, r5);
-    *acc2 = _mm256_add_ps(*acc2, r6);
-    *acc3 = _mm256_add_ps(*acc3, r7);
-    *acc4 = _mm256_add_ps(*acc4, r8);
-
-    *norm_x_acc1 = _mm256_add_ps(*norm_x_acc1, norm_x_r1);
-    *norm_x_acc2 = _mm256_add_ps(*norm_x_acc2, norm_x_r2);
-    *norm_x_acc3 = _mm256_add_ps(*norm_x_acc3, norm_x_r3);
-    *norm_x_acc4 = _mm256_add_ps(*norm_x_acc4, norm_x_r4);
-    *norm_x_acc1 = _mm256_add_ps(*norm_x_acc1, norm_x_r5);
-    *norm_x_acc2 = _mm256_add_ps(*norm_x_acc2, norm_x_r6);
-    *norm_x_acc3 = _mm256_add_ps(*norm_x_acc3, norm_x_r7);
-    *norm_x_acc4 = _mm256_add_ps(*norm_x_acc4, norm_x_r8);
-
-    *norm_y_acc1 = _mm256_add_ps(*norm_y_acc1, norm_y_r1);
-    *norm_y_acc2 = _mm256_add_ps(*norm_y_acc2, norm_y_r2);
-    *norm_y_acc3 = _mm256_add_ps(*norm_y_acc3, norm_y_r3);
-    *norm_y_acc4 = _mm256_add_ps(*norm_y_acc4, norm_y_r4);
-    *norm_y_acc1 = _mm256_add_ps(*norm_y_acc1, norm_y_r5);
-    *norm_y_acc2 = _mm256_add_ps(*norm_y_acc2, norm_y_r6);
-    *norm_y_acc3 = _mm256_add_ps(*norm_y_acc3, norm_y_r7);
-    *norm_y_acc4 = _mm256_add_ps(*norm_y_acc4, norm_y_r8);
-}
-
-#[allow(clippy::too_many_arguments)]
-#[inline(always)]
-unsafe fn execute_f32_x64_fma_block_cosine(
-    x: *const f32,
-    y: *const f32,
-    acc1: &mut __m256,
-    acc2: &mut __m256,
-    acc3: &mut __m256,
-    acc4: &mut __m256,
-    norm_x_acc1: &mut __m256,
-    norm_x_acc2: &mut __m256,
-    norm_x_acc3: &mut __m256,
-    norm_x_acc4: &mut __m256,
-    norm_y_acc1: &mut __m256,
-    norm_y_acc2: &mut __m256,
-    norm_y_acc3: &mut __m256,
-    norm_y_acc4: &mut __m256,
-) {
-    let [x1, x2, x3, x4] = offsets(x, 0);
-    let [x5, x6, x7, x8] = offsets(x, 32);
-
-    let [y1, y2, y3, y4] = offsets(y, 0);
-    let [y5, y6, y7, y8] = offsets(y, 32);
-
-    let x1 = _mm256_loadu_ps(x1);
-    let x2 = _mm256_loadu_ps(x2);
-    let x3 = _mm256_loadu_ps(x3);
-    let x4 = _mm256_loadu_ps(x4);
-    let x5 = _mm256_loadu_ps(x5);
-    let x6 = _mm256_loadu_ps(x6);
-    let x7 = _mm256_loadu_ps(x7);
-    let x8 = _mm256_loadu_ps(x8);
-
-    let y1 = _mm256_loadu_ps(y1);
-    let y2 = _mm256_loadu_ps(y2);
-    let y3 = _mm256_loadu_ps(y3);
-    let y4 = _mm256_loadu_ps(y4);
-    let y5 = _mm256_loadu_ps(y5);
-    let y6 = _mm256_loadu_ps(y6);
-    let y7 = _mm256_loadu_ps(y7);
-    let y8 = _mm256_loadu_ps(y8);
-
-    *acc1 = _mm256_fmadd_ps(x1, y1, *acc1);
-    *acc2 = _mm256_fmadd_ps(x2, y2, *acc2);
-    *acc3 = _mm256_fmadd_ps(x3, y3, *acc3);
-    *acc4 = _mm256_fmadd_ps(x4, y4, *acc4);
-    *acc1 = _mm256_fmadd_ps(x5, y5, *acc1);
-    *acc2 = _mm256_fmadd_ps(x6, y6, *acc2);
-    *acc3 = _mm256_fmadd_ps(x7, y7, *acc3);
-    *acc4 = _mm256_fmadd_ps(x8, y8, *acc4);
-
-    *norm_x_acc1 = _mm256_fmadd_ps(x1, x1, *norm_x_acc1);
-    *norm_x_acc2 = _mm256_fmadd_ps(x2, x2, *norm_x_acc2);
-    *norm_x_acc3 = _mm256_fmadd_ps(x3, x3, *norm_x_acc3);
-    *norm_x_acc4 = _mm256_fmadd_ps(x4, x4, *norm_x_acc4);
-    *norm_x_acc1 = _mm256_fmadd_ps(x5, x5, *norm_x_acc1);
-    *norm_x_acc2 = _mm256_fmadd_ps(x6, x6, *norm_x_acc2);
-    *norm_x_acc3 = _mm256_fmadd_ps(x7, x7, *norm_x_acc3);
-    *norm_x_acc4 = _mm256_fmadd_ps(x8, x8, *norm_x_acc4);
-
-    *norm_y_acc1 = _mm256_fmadd_ps(y1, y1, *norm_y_acc1);
-    *norm_y_acc2 = _mm256_fmadd_ps(y2, y2, *norm_y_acc2);
-    *norm_y_acc3 = _mm256_fmadd_ps(y3, y3, *norm_y_acc3);
-    *norm_y_acc4 = _mm256_fmadd_ps(y4, y4, *norm_y_acc4);
-    *norm_y_acc1 = _mm256_fmadd_ps(y5, y5, *norm_y_acc1);
-    *norm_y_acc2 = _mm256_fmadd_ps(y6, y6, *norm_y_acc2);
-    *norm_y_acc3 = _mm256_fmadd_ps(y7, y7, *norm_y_acc3);
-    *norm_y_acc4 = _mm256_fmadd_ps(y8, y8, *norm_y_acc4);
-}
-
 macro_rules! unrolled_loop {
     (
         $executor:ident,
@@ -613,4 +431,186 @@ pub unsafe fn f32_x512_avx2_fma_cosine(x: &[f32], y: &[f32]) -> f32 {
     let norm_y_acc = rollup_x4(norm_y_acc1, norm_y_acc2, norm_y_acc3, norm_y_acc4);
 
     cosine::<FastMath>(acc, norm_x_acc, norm_y_acc)
+}
+
+#[inline(always)]
+unsafe fn cosine<M: Math>(acc: __m256, norm_x_acc: __m256, norm_y_acc: __m256) -> f32 {
+    let dot_product = sum_avx2(acc);
+    let norm_x = sum_avx2(norm_x_acc);
+    let norm_y = sum_avx2(norm_y_acc);
+
+    if norm_x == 0.0 && norm_y == 0.0 {
+        0.0
+    } else if norm_x == 0.0 || norm_y == 0.0 {
+        1.0
+    } else {
+        M::sub(1.0, M::div(dot_product, M::mul(norm_x, norm_y).sqrt()))
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+#[inline(always)]
+unsafe fn execute_f32_x64_nofma_block_cosine(
+    x: *const f32,
+    y: *const f32,
+    acc1: &mut __m256,
+    acc2: &mut __m256,
+    acc3: &mut __m256,
+    acc4: &mut __m256,
+    norm_x_acc1: &mut __m256,
+    norm_x_acc2: &mut __m256,
+    norm_x_acc3: &mut __m256,
+    norm_x_acc4: &mut __m256,
+    norm_y_acc1: &mut __m256,
+    norm_y_acc2: &mut __m256,
+    norm_y_acc3: &mut __m256,
+    norm_y_acc4: &mut __m256,
+) {
+    let [x1, x2, x3, x4] = offsets(x, 0);
+    let [x5, x6, x7, x8] = offsets(x, 32);
+
+    let [y1, y2, y3, y4] = offsets(y, 0);
+    let [y5, y6, y7, y8] = offsets(y, 32);
+
+    let x1 = _mm256_loadu_ps(x1);
+    let x2 = _mm256_loadu_ps(x2);
+    let x3 = _mm256_loadu_ps(x3);
+    let x4 = _mm256_loadu_ps(x4);
+    let x5 = _mm256_loadu_ps(x5);
+    let x6 = _mm256_loadu_ps(x6);
+    let x7 = _mm256_loadu_ps(x7);
+    let x8 = _mm256_loadu_ps(x8);
+
+    let y1 = _mm256_loadu_ps(y1);
+    let y2 = _mm256_loadu_ps(y2);
+    let y3 = _mm256_loadu_ps(y3);
+    let y4 = _mm256_loadu_ps(y4);
+    let y5 = _mm256_loadu_ps(y5);
+    let y6 = _mm256_loadu_ps(y6);
+    let y7 = _mm256_loadu_ps(y7);
+    let y8 = _mm256_loadu_ps(y8);
+
+    let r1 = _mm256_mul_ps(x1, y1);
+    let r2 = _mm256_mul_ps(x2, y2);
+    let r3 = _mm256_mul_ps(x3, y3);
+    let r4 = _mm256_mul_ps(x4, y4);
+    let r5 = _mm256_mul_ps(x5, y5);
+    let r6 = _mm256_mul_ps(x6, y6);
+    let r7 = _mm256_mul_ps(x7, y7);
+    let r8 = _mm256_mul_ps(x8, y8);
+
+    let norm_x_r1 = _mm256_mul_ps(x1, x1);
+    let norm_x_r2 = _mm256_mul_ps(x2, x2);
+    let norm_x_r3 = _mm256_mul_ps(x3, x3);
+    let norm_x_r4 = _mm256_mul_ps(x4, x4);
+    let norm_x_r5 = _mm256_mul_ps(x5, x5);
+    let norm_x_r6 = _mm256_mul_ps(x6, x6);
+    let norm_x_r7 = _mm256_mul_ps(x7, x7);
+    let norm_x_r8 = _mm256_mul_ps(x8, x8);
+
+    let norm_y_r1 = _mm256_mul_ps(y1, y1);
+    let norm_y_r2 = _mm256_mul_ps(y2, y2);
+    let norm_y_r3 = _mm256_mul_ps(y3, y3);
+    let norm_y_r4 = _mm256_mul_ps(y4, y4);
+    let norm_y_r5 = _mm256_mul_ps(y5, y5);
+    let norm_y_r6 = _mm256_mul_ps(y6, y6);
+    let norm_y_r7 = _mm256_mul_ps(y7, y7);
+    let norm_y_r8 = _mm256_mul_ps(y8, y8);
+
+    *acc1 = _mm256_add_ps(*acc1, r1);
+    *acc2 = _mm256_add_ps(*acc2, r2);
+    *acc3 = _mm256_add_ps(*acc3, r3);
+    *acc4 = _mm256_add_ps(*acc4, r4);
+    *acc1 = _mm256_add_ps(*acc1, r5);
+    *acc2 = _mm256_add_ps(*acc2, r6);
+    *acc3 = _mm256_add_ps(*acc3, r7);
+    *acc4 = _mm256_add_ps(*acc4, r8);
+
+    *norm_x_acc1 = _mm256_add_ps(*norm_x_acc1, norm_x_r1);
+    *norm_x_acc2 = _mm256_add_ps(*norm_x_acc2, norm_x_r2);
+    *norm_x_acc3 = _mm256_add_ps(*norm_x_acc3, norm_x_r3);
+    *norm_x_acc4 = _mm256_add_ps(*norm_x_acc4, norm_x_r4);
+    *norm_x_acc1 = _mm256_add_ps(*norm_x_acc1, norm_x_r5);
+    *norm_x_acc2 = _mm256_add_ps(*norm_x_acc2, norm_x_r6);
+    *norm_x_acc3 = _mm256_add_ps(*norm_x_acc3, norm_x_r7);
+    *norm_x_acc4 = _mm256_add_ps(*norm_x_acc4, norm_x_r8);
+
+    *norm_y_acc1 = _mm256_add_ps(*norm_y_acc1, norm_y_r1);
+    *norm_y_acc2 = _mm256_add_ps(*norm_y_acc2, norm_y_r2);
+    *norm_y_acc3 = _mm256_add_ps(*norm_y_acc3, norm_y_r3);
+    *norm_y_acc4 = _mm256_add_ps(*norm_y_acc4, norm_y_r4);
+    *norm_y_acc1 = _mm256_add_ps(*norm_y_acc1, norm_y_r5);
+    *norm_y_acc2 = _mm256_add_ps(*norm_y_acc2, norm_y_r6);
+    *norm_y_acc3 = _mm256_add_ps(*norm_y_acc3, norm_y_r7);
+    *norm_y_acc4 = _mm256_add_ps(*norm_y_acc4, norm_y_r8);
+}
+
+#[allow(clippy::too_many_arguments)]
+#[inline(always)]
+unsafe fn execute_f32_x64_fma_block_cosine(
+    x: *const f32,
+    y: *const f32,
+    acc1: &mut __m256,
+    acc2: &mut __m256,
+    acc3: &mut __m256,
+    acc4: &mut __m256,
+    norm_x_acc1: &mut __m256,
+    norm_x_acc2: &mut __m256,
+    norm_x_acc3: &mut __m256,
+    norm_x_acc4: &mut __m256,
+    norm_y_acc1: &mut __m256,
+    norm_y_acc2: &mut __m256,
+    norm_y_acc3: &mut __m256,
+    norm_y_acc4: &mut __m256,
+) {
+    let [x1, x2, x3, x4] = offsets(x, 0);
+    let [x5, x6, x7, x8] = offsets(x, 32);
+
+    let [y1, y2, y3, y4] = offsets(y, 0);
+    let [y5, y6, y7, y8] = offsets(y, 32);
+
+    let x1 = _mm256_loadu_ps(x1);
+    let x2 = _mm256_loadu_ps(x2);
+    let x3 = _mm256_loadu_ps(x3);
+    let x4 = _mm256_loadu_ps(x4);
+    let x5 = _mm256_loadu_ps(x5);
+    let x6 = _mm256_loadu_ps(x6);
+    let x7 = _mm256_loadu_ps(x7);
+    let x8 = _mm256_loadu_ps(x8);
+
+    let y1 = _mm256_loadu_ps(y1);
+    let y2 = _mm256_loadu_ps(y2);
+    let y3 = _mm256_loadu_ps(y3);
+    let y4 = _mm256_loadu_ps(y4);
+    let y5 = _mm256_loadu_ps(y5);
+    let y6 = _mm256_loadu_ps(y6);
+    let y7 = _mm256_loadu_ps(y7);
+    let y8 = _mm256_loadu_ps(y8);
+
+    *acc1 = _mm256_fmadd_ps(x1, y1, *acc1);
+    *acc2 = _mm256_fmadd_ps(x2, y2, *acc2);
+    *acc3 = _mm256_fmadd_ps(x3, y3, *acc3);
+    *acc4 = _mm256_fmadd_ps(x4, y4, *acc4);
+    *acc1 = _mm256_fmadd_ps(x5, y5, *acc1);
+    *acc2 = _mm256_fmadd_ps(x6, y6, *acc2);
+    *acc3 = _mm256_fmadd_ps(x7, y7, *acc3);
+    *acc4 = _mm256_fmadd_ps(x8, y8, *acc4);
+
+    *norm_x_acc1 = _mm256_fmadd_ps(x1, x1, *norm_x_acc1);
+    *norm_x_acc2 = _mm256_fmadd_ps(x2, x2, *norm_x_acc2);
+    *norm_x_acc3 = _mm256_fmadd_ps(x3, x3, *norm_x_acc3);
+    *norm_x_acc4 = _mm256_fmadd_ps(x4, x4, *norm_x_acc4);
+    *norm_x_acc1 = _mm256_fmadd_ps(x5, x5, *norm_x_acc1);
+    *norm_x_acc2 = _mm256_fmadd_ps(x6, x6, *norm_x_acc2);
+    *norm_x_acc3 = _mm256_fmadd_ps(x7, x7, *norm_x_acc3);
+    *norm_x_acc4 = _mm256_fmadd_ps(x8, x8, *norm_x_acc4);
+
+    *norm_y_acc1 = _mm256_fmadd_ps(y1, y1, *norm_y_acc1);
+    *norm_y_acc2 = _mm256_fmadd_ps(y2, y2, *norm_y_acc2);
+    *norm_y_acc3 = _mm256_fmadd_ps(y3, y3, *norm_y_acc3);
+    *norm_y_acc4 = _mm256_fmadd_ps(y4, y4, *norm_y_acc4);
+    *norm_y_acc1 = _mm256_fmadd_ps(y5, y5, *norm_y_acc1);
+    *norm_y_acc2 = _mm256_fmadd_ps(y6, y6, *norm_y_acc2);
+    *norm_y_acc3 = _mm256_fmadd_ps(y7, y7, *norm_y_acc3);
+    *norm_y_acc4 = _mm256_fmadd_ps(y8, y8, *norm_y_acc4);
 }
--- igris-accel/src/danger/avx2_euclidean.rs
+++ igris-accel/src/danger/avx2_euclidean.rs
@@ -2,129 +2,6 @@ use std::arch::x86_64::*;
 
 use crate::danger::{offsets, rollup_x8, sum_avx2};
 
-#[allow(clippy::too_many_arguments)]
-#[inline(always)]
-unsafe fn execute_f32_x64_nofma_block_euclidean(
-    x: *const f32,
-    y: *const f32,
-    acc1: &mut __m256,
-    acc2: &mut __m256,
-    acc3: &mut __m256,
-    acc4: &mut __m256,
-    acc5: &mut __m256,
-    acc6: &mut __m256,
-    acc7: &mut __m256,
-    acc8: &mut __m256,
-) {
-    let [x1, x2, x3, x4] = offsets(x, 0);
-    let [x5, x6, x7, x8] = offsets(x, 32);
-
-    let [y1, y2, y3, y4] = offsets(y, 0);
-    let [y5, y6, y7, y8] = offsets(y, 32);
-
-    let x1 = _mm256_loadu_ps(x1);
-    let x2 = _mm256_loadu_ps(x2);
-    let x3 = _mm256_loadu_ps(x3);
-    let x4 = _mm256_loadu_ps(x4);
-    let x5 = _mm256_loadu_ps(x5);
-    let x6 = _mm256_loadu_ps(x6);
-    let x7 = _mm256_loadu_ps(x7);
-    let x8 = _mm256_loadu_ps(x8);
-
-    let y1 = _mm256_loadu_ps(y1);
-    let y2 = _mm256_loadu_ps(y2);
-    let y3 = _mm256_loadu_ps(y3);
-    let y4 = _mm256_loadu_ps(y4);
-    let y5 = _mm256_loadu_ps(y5);
-    let y6 = _mm256_loadu_ps(y6);
-    let y7 = _mm256_loadu_ps(y7);
-    let y8 = _mm256_loadu_ps(y8);
-
-    let diff1 = _mm256_sub_ps(x1, y1);
-    let diff2 = _mm256_sub_ps(x2, y2);
-    let diff3 = _mm256_sub_ps(x3, y3);
-    let diff4 = _mm256_sub_ps(x4, y4);
-    let diff5 = _mm256_sub_ps(x5, y5);
-    let diff6 = _mm256_sub_ps(x6, y6);
-    let diff7 = _mm256_sub_ps(x7, y7);
-    let diff8 = _mm256_sub_ps(x8, y8);
-
-    let r1 = _mm256_mul_ps(diff1, diff1);
-    let r2 = _mm256_mul_ps(diff2, diff2);
-    let r3 = _mm256_mul_ps(diff3, diff3);
-    let r4 = _mm256_mul_ps(diff4, diff4);
-    let r5 = _mm256_mul_ps(diff5, diff5);
-    let r6 = _mm256_mul_ps(diff6, diff6);
-    let r7 = _mm256_mul_ps(diff7, diff7);
-    let r8 = _mm256_mul_ps(diff8, diff8);
-
-    *acc1 = _mm256_add_ps(*acc1, r1);
-    *acc2 = _mm256_add_ps(*acc2, r2);
-    *acc3 = _mm256_add_ps(*acc3, r3);
-    *acc4 = _mm256_add_ps(*acc4, r4);
-    *acc5 = _mm256_add_ps(*acc5, r5);
-    *acc6 = _mm256_add_ps(*acc6, r6);
-    *acc7 = _mm256_add_ps(*acc7, r7);
-    *acc8 = _mm256_add_ps(*acc8, r8);
-}
-
-#[allow(clippy::too_many_arguments)]
-#[inline(always)]
-unsafe fn execute_f32_x64_fma_block_euclidean(
-    x: *const f32,
-    y: *const f32,
-    acc1: &mut __m256,
-    acc2: &mut __m256,
-    acc3: &mut __m256,
-    acc4: &mut __m256,
-    acc5: &mut __m256,
-    acc6: &mut __m256,
-    acc7: &mut __m256,
-    acc8: &mut __m256,
-) {
-    let [x1, x2, x3, x4] = offsets(x, 0);
-    let [x5, x6, x7, x8] = offsets(x, 32);
-
-    let [y1, y2, y3, y4] = offsets(y, 0);
-    let [y5, y6, y7, y8] = offsets(y, 32);
-
-    let x1 = _mm256_loadu_ps(x1);
-    let x2 = _mm256_loadu_ps(x2);
-    let x3 = _mm256_loadu_ps(x3);
-    let x4 = _mm256_loadu_ps(x4);
-    let x5 = _mm256_loadu_ps(x5);
-    let x6 = _mm256_loadu_ps(x6);
-    let x7 = _mm256_loadu_ps(x7);
-    let x8 = _mm256_loadu_ps(x8);
-
-    let y1 = _mm256_loadu_ps(y1);
-    let y2 = _mm256_loadu_ps(y2);
-    let y3 = _mm256_loadu_ps(y3);
-    let y4 = _mm256_loadu_ps(y4);
-    let y5 = _mm256_loadu_ps(y5);
-    let y6 = _mm256_loadu_ps(y6);
-    let y7 = _mm256_loadu_ps(y7);
-    let y8 = _mm256_loadu_ps(y8);
-
-    let diff1 = _mm256_sub_ps(x1, y1);
-    let diff2 = _mm256_sub_ps(x2, y2);
-    let diff3 = _mm256_sub_ps(x3, y3);
-    let diff4 = _mm256_sub_ps(x4, y4);
-    let diff5 = _mm256_sub_ps(x5, y5);
-    let diff6 = _mm256_sub_ps(x6, y6);
-    let diff7 = _mm256_sub_ps(x7, y7);
-    let diff8 = _mm256_sub_ps(x8, y8);
-
-    *acc1 = _mm256_fmadd_ps(diff1, diff1, *acc1);
-    *acc2 = _mm256_fmadd_ps(diff2, diff2, *acc2);
-    *acc3 = _mm256_fmadd_ps(diff3, diff3, *acc3);
-    *acc4 = _mm256_fmadd_ps(diff4, diff4, *acc4);
-    *acc5 = _mm256_fmadd_ps(diff5, diff5, *acc5);
-    *acc6 = _mm256_fmadd_ps(diff6, diff6, *acc6);
-    *acc7 = _mm256_fmadd_ps(diff7, diff7, *acc7);
-    *acc8 = _mm256_fmadd_ps(diff8, diff8, *acc8);
-}
-
 macro_rules! unrolled_loop {
     (
         $executor:ident,
@@ -467,4 +344,127 @@ pub unsafe fn f32_x512_avx2_fma_euclidean(x: &[f32], y: &[f32]) -> f32 {
 
     let acc = rollup_x8(acc1, acc2, acc3, acc4, acc5, acc6, acc7, acc8);
     sum_avx2(acc)
+}
+
+#[allow(clippy::too_many_arguments)]
+#[inline(always)]
+unsafe fn execute_f32_x64_nofma_block_euclidean(
+    x: *const f32,
+    y: *const f32,
+    acc1: &mut __m256,
+    acc2: &mut __m256,
+    acc3: &mut __m256,
+    acc4: &mut __m256,
+    acc5: &mut __m256,
+    acc6: &mut __m256,
+    acc7: &mut __m256,
+    acc8: &mut __m256,
+) {
+    let [x1, x2, x3, x4] = offsets(x, 0);
+    let [x5, x6, x7, x8] = offsets(x, 32);
+
+    let [y1, y2, y3, y4] = offsets(y, 0);
+    let [y5, y6, y7, y8] = offsets(y, 32);
+
+    let x1 = _mm256_loadu_ps(x1);
+    let x2 = _mm256_loadu_ps(x2);
+    let x3 = _mm256_loadu_ps(x3);
+    let x4 = _mm256_loadu_ps(x4);
+    let x5 = _mm256_loadu_ps(x5);
+    let x6 = _mm256_loadu_ps(x6);
+    let x7 = _mm256_loadu_ps(x7);
+    let x8 = _mm256_loadu_ps(x8);
+
+    let y1 = _mm256_loadu_ps(y1);
+    let y2 = _mm256_loadu_ps(y2);
+    let y3 = _mm256_loadu_ps(y3);
+    let y4 = _mm256_loadu_ps(y4);
+    let y5 = _mm256_loadu_ps(y5);
+    let y6 = _mm256_loadu_ps(y6);
+    let y7 = _mm256_loadu_ps(y7);
+    let y8 = _mm256_loadu_ps(y8);
+
+    let diff1 = _mm256_sub_ps(x1, y1);
+    let diff2 = _mm256_sub_ps(x2, y2);
+    let diff3 = _mm256_sub_ps(x3, y3);
+    let diff4 = _mm256_sub_ps(x4, y4);
+    let diff5 = _mm256_sub_ps(x5, y5);
+    let diff6 = _mm256_sub_ps(x6, y6);
+    let diff7 = _mm256_sub_ps(x7, y7);
+    let diff8 = _mm256_sub_ps(x8, y8);
+
+    let r1 = _mm256_mul_ps(diff1, diff1);
+    let r2 = _mm256_mul_ps(diff2, diff2);
+    let r3 = _mm256_mul_ps(diff3, diff3);
+    let r4 = _mm256_mul_ps(diff4, diff4);
+    let r5 = _mm256_mul_ps(diff5, diff5);
+    let r6 = _mm256_mul_ps(diff6, diff6);
+    let r7 = _mm256_mul_ps(diff7, diff7);
+    let r8 = _mm256_mul_ps(diff8, diff8);
+
+    *acc1 = _mm256_add_ps(*acc1, r1);
+    *acc2 = _mm256_add_ps(*acc2, r2);
+    *acc3 = _mm256_add_ps(*acc3, r3);
+    *acc4 = _mm256_add_ps(*acc4, r4);
+    *acc5 = _mm256_add_ps(*acc5, r5);
+    *acc6 = _mm256_add_ps(*acc6, r6);
+    *acc7 = _mm256_add_ps(*acc7, r7);
+    *acc8 = _mm256_add_ps(*acc8, r8);
+}
+
+#[allow(clippy::too_many_arguments)]
+#[inline(always)]
+unsafe fn execute_f32_x64_fma_block_euclidean(
+    x: *const f32,
+    y: *const f32,
+    acc1: &mut __m256,
+    acc2: &mut __m256,
+    acc3: &mut __m256,
+    acc4: &mut __m256,
+    acc5: &mut __m256,
+    acc6: &mut __m256,
+    acc7: &mut __m256,
+    acc8: &mut __m256,
+) {
+    let [x1, x2, x3, x4] = offsets(x, 0);
+    let [x5, x6, x7, x8] = offsets(x, 32);
+
+    let [y1, y2, y3, y4] = offsets(y, 0);
+    let [y5, y6, y7, y8] = offsets(y, 32);
+
+    let x1 = _mm256_loadu_ps(x1);
+    let x2 = _mm256_loadu_ps(x2);
+    let x3 = _mm256_loadu_ps(x3);
+    let x4 = _mm256_loadu_ps(x4);
+    let x5 = _mm256_loadu_ps(x5);
+    let x6 = _mm256_loadu_ps(x6);
+    let x7 = _mm256_loadu_ps(x7);
+    let x8 = _mm256_loadu_ps(x8);
+
+    let y1 = _mm256_loadu_ps(y1);
+    let y2 = _mm256_loadu_ps(y2);
+    let y3 = _mm256_loadu_ps(y3);
+    let y4 = _mm256_loadu_ps(y4);
+    let y5 = _mm256_loadu_ps(y5);
+    let y6 = _mm256_loadu_ps(y6);
+    let y7 = _mm256_loadu_ps(y7);
+    let y8 = _mm256_loadu_ps(y8);
+
+    let diff1 = _mm256_sub_ps(x1, y1);
+    let diff2 = _mm256_sub_ps(x2, y2);
+    let diff3 = _mm256_sub_ps(x3, y3);
+    let diff4 = _mm256_sub_ps(x4, y4);
+    let diff5 = _mm256_sub_ps(x5, y5);
+    let diff6 = _mm256_sub_ps(x6, y6);
+    let diff7 = _mm256_sub_ps(x7, y7);
+    let diff8 = _mm256_sub_ps(x8, y8);
+
+    *acc1 = _mm256_fmadd_ps(diff1, diff1, *acc1);
+    *acc2 = _mm256_fmadd_ps(diff2, diff2, *acc2);
+    *acc3 = _mm256_fmadd_ps(diff3, diff3, *acc3);
+    *acc4 = _mm256_fmadd_ps(diff4, diff4, *acc4);
+    *acc5 = _mm256_fmadd_ps(diff5, diff5, *acc5);
+    *acc6 = _mm256_fmadd_ps(diff6, diff6, *acc6);
+    *acc7 = _mm256_fmadd_ps(diff7, diff7, *acc7);
+    *acc8 = _mm256_fmadd_ps(diff8, diff8, *acc8);
 }
