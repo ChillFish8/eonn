21ab9508d6cc87c9d4cadbf749d89b8a6972bc39
chillfish8 <hburt2003@gmail.com>
2024-05-11 01:08:15 +0200

    Move to `fop_algebraic` instead of `fop_fast` to prevent UB in safe code.

--- eonn-accel/src/math/fast_math.rs
+++ eonn-accel/src/math/fast_math.rs
@@ -3,38 +3,26 @@ use core::intrinsics;
 use super::Math;
 
 /// Basic math operations backed by fast-math intrinsics.
-///
-/// WARNING:
-/// These operations can assume all values are finite, there is no
-/// guaranteed behaviour if this condition is breached.
 pub struct FastMath;
 
 impl Math for FastMath {
     #[inline(always)]
     fn add(a: f32, b: f32) -> f32 {
-        debug_assert!(a.is_finite());
-        debug_assert!(b.is_finite());
-        unsafe { intrinsics::fadd_fast(a, b) }
+        intrinsics::fadd_algebraic(a, b)
     }
 
     #[inline(always)]
     fn sub(a: f32, b: f32) -> f32 {
-        debug_assert!(a.is_finite());
-        debug_assert!(b.is_finite());
-        unsafe { intrinsics::fsub_fast(a, b) }
+        intrinsics::fsub_algebraic(a, b)
     }
 
     #[inline(always)]
     fn mul(a: f32, b: f32) -> f32 {
-        debug_assert!(a.is_finite());
-        debug_assert!(b.is_finite());
-        unsafe { intrinsics::fmul_fast(a, b) }
+        intrinsics::fmul_algebraic(a, b)
     }
 
     #[inline(always)]
     fn div(a: f32, b: f32) -> f32 {
-        debug_assert!(a.is_finite());
-        debug_assert!(b.is_finite());
-        unsafe { intrinsics::fdiv_fast(a, b) }
+        intrinsics::fdiv_algebraic(a, b)
     }
 }
