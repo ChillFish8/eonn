69a7835896a5ba6fe544b62b5725b033c02fe46f
chillfish8 <hburt2003@gmail.com>
2024-05-11 03:43:32 +0200

    Auto gen exports for asm

--- eonn-accel/asm-view/src/lib.rs
+++ eonn-accel/asm-view/src/lib.rs
@@ -1,31 +1,48 @@
 #![allow(clippy::missing_safety_doc)]
 
-#[no_mangle]
-#[inline(never)]
-pub unsafe fn xany_avx2_nofma_dot(x: &[f32], y: &[f32]) -> f32 {
-    eonn_accel::danger::f32_xany_avx2_nofma_dot(x, y)
+
+macro_rules! define_distance_exports {
+    ($op:ident) => {
+        paste::paste! {            
+            #[no_mangle]
+            #[inline(never)]
+            pub unsafe fn [< xany_avx2_nofma_ $op >] (x: &[f32], y: &[f32]) -> f32 {
+                eonn_accel::danger::[< f32_xany_avx2_nofma_ $op >](x, y)
+            }
+            
+            #[no_mangle]
+            #[inline(never)]
+            pub unsafe fn [< xconst_avx2_nofma_  $op >](x: &[f32], y: &[f32]) -> f32 {
+                eonn_accel::danger::[< f32_xconst_avx2_nofma_  $op >]::<1024>(x, y)
+            }
+            
+            #[no_mangle]
+            #[inline(never)]
+            pub unsafe fn [< xany_avx2_fma_ $op >](x: &[f32], y: &[f32]) -> f32 {
+                eonn_accel::danger::[< f32_xany_avx2_fma_ $op >](x, y)
+            }
+            
+            #[no_mangle]
+            #[inline(never)]
+            pub unsafe fn [< xconst_avx2_fma_ $op >](x: &[f32], y: &[f32]) -> f32 {
+                eonn_accel::danger::[< f32_xconst_avx2_fma_ $op >]::<1024>(x, y)
+            }
+            
+            #[no_mangle]
+            #[inline(never)]
+            pub unsafe fn [< xany_avx512_fma_ $op >](x: &[f32], y: &[f32]) -> f32 {
+                eonn_accel::danger::[< f32_xany_avx512_fma_ $op >](x, y)
+            }
+            
+            #[no_mangle]
+            #[inline(never)]
+            pub unsafe fn [< xconst_avx512_fma_ $op >](x: &[f32], y: &[f32]) -> f32 {
+                eonn_accel::danger::[< f32_xconst_avx512_fma_ $op >]::<1024>(x, y)
+            }
+        }
+    };
 }
 
-#[no_mangle]
-#[inline(never)]
-pub unsafe fn xany_avx2_fma_dot(x: &[f32], y: &[f32]) -> f32 {
-    eonn_accel::danger::f32_xany_avx2_fma_dot(x, y)
-}
-
-#[no_mangle]
-#[inline(never)]
-pub unsafe fn xconst_avx2_fma_dot(x: &[f32], y: &[f32]) -> f32 {
-    eonn_accel::danger::f32_xconst_avx2_fma_dot::<1024>(x, y)
-}
-
-#[no_mangle]
-#[inline(never)]
-pub unsafe fn xconst_avx2_nofma_dot(x: &[f32], y: &[f32]) -> f32 {
-    eonn_accel::danger::f32_xconst_avx2_nofma_dot::<1024>(x, y)
-}
-
-#[no_mangle]
-#[inline(never)]
-pub unsafe fn xany_avx2_nofma_sum(x: &[f32]) -> f32 {
-    eonn_accel::danger::f32_xany_avx2_nofma_sum_horizontal(x)
-}
+define_distance_exports!(dot);
+define_distance_exports!(cosine);
+define_distance_exports!(euclidean);
